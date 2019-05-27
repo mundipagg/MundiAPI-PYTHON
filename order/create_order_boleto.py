b8f7d1767edf983cee67504c7be5bbd6aaf08cb4
Felipe Paixão <felipe.paixao@mundipagg.com>
2019-05-27 16:25:10 -0300

    - Inclui arquivos de exemplo e testes para criação de Pedidos

---
+++ order/create_order_boleto.py
@@ -0,0 +1,58 @@
+from mundiapi.mundiapi_client import MundiapiClient
+from mundiapi.models import *
+from mundiapi.controllers import *
+from mundiapi.exceptions.error_exception import *
+
+MundiapiClient.config.basic_auth_user_name = "SUA_SECRET_KEY:"
+orders_controller = orders_controller.OrdersController()
+
+customer = create_customer_request.CreateCustomerRequest()
+customer.name = "sdk customer test"
+customer.email = "tonystark@avengers.com"
+customer.address = create_address_request.CreateAddressRequest()
+customer.address.street = "Malibu Point"
+customer.address.number = "10880"
+customer.address.zip_code = "90265"
+customer.address.neighborhood = "Central Malibu"
+customer.address.city = "Malibu"
+customer.address.state = "CA"
+customer.address.country = "US"
+
+boleto = create_boleto_payment_request.CreateBoletoPaymentRequest()
+boleto.bank = "033"
+boleto.instructions = "Pagar até o vencimento"
+boleto.due_at = '2019-12-31T00:00:00Z'
+
+request = create_order_request.CreateOrderRequest()
+
+request.items = [create_order_item_request.CreateOrderItemRequest()]
+request.items[0].description = "Tesseract Bracelet"
+request.items[0].quantity = 3
+request.items[0].amount = 1490
+
+request.payments = [create_payment_request.CreatePaymentRequest()]
+request.payments[0].payment_method = "boleto"
+request.payments[0].boleto = boleto
+request.customer = customer
+
+try:
+    result = orders_controller.create_order(request)
+    assert result.status == "pending"
+    assert result.charges[0].status == "pending"
+    assert result.charges[0].last_transaction.success == True
+    assert result.charges[0].last_transaction.status == "generated"
+    assert result.charges[0].last_transaction.url is not None
+    assert result.charges[0].last_transaction.bank is not None
+    assert result.charges[0].last_transaction.pdf is not None
+    assert result.charges[0].last_transaction.instructions is not None
+    print("Order result status: ", result.status)
+    print("Boleto pdf: ", result.charges[0].last_transaction.pdf)
+except ErrorException as ex:
+    print(ex.message)
+    print("Errors: ", ex.errors)
+except Exception as ex:
+    raise ex
+
+
+
+
---
+++ order/create_order_voucher.py
@@ -0,0 +1,48 @@
+from mundiapi.mundiapi_client import MundiapiClient
+from mundiapi.models import *
+from mundiapi.controllers import *
+from mundiapi.exceptions.error_exception import *
+
+MundiapiClient.config.basic_auth_user_name = "SUA_SECRET_KEY:"
+orders_controller = orders_controller.OrdersController()
+
+customer = create_customer_request.CreateCustomerRequest()
+customer.name = "sdk customer test"
+customer.email = "tonystark@avengers.com"
+
+voucher = create_voucher_payment_request.CreateVoucherPaymentRequest()
+voucher.capture = True
+voucher.installments = 2
+voucher.statement_descriptor = "test descriptor"
+voucher.card = create_card_request.CreateCardRequest()
+voucher.card.number = "4000000000000010"
+voucher.card.holder_name = "Tony Stark"
+voucher.card.exp_month = 1
+voucher.card.exp_year = 2025
+voucher.card.cvv = "123"
+
+request = create_order_request.CreateOrderRequest()
+
+request.items = [create_order_item_request.CreateOrderItemRequest()]
+request.items[0].description = "Tesseract Bracelet"
+request.items[0].quantity = 3
+request.items[0].amount = 1490
+
+request.payments = [create_payment_request.CreatePaymentRequest()]
+request.payments[0].payment_method = "voucher"
+request.payments[0].voucher = voucher
+request.customer = customer
+
+try:
+    result = orders_controller.create_order(request)
+    assert result.status == "paid"
+    print("Order result status: ", result.status)
+except ErrorException as ex:
+    print(ex.message)
+    print("Errors: ", ex.errors)
+except Exception as ex:
+    raise ex
+
+
+
+
