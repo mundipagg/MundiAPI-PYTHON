376b35a644f78d2ec182e265a37e5dc86aa93782
Marcelo Marini <marcelomarini17@hotmail.com>
2020-11-16 20:30:48 +0000

    :art: Add pix payment

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='mundiapi',
-    version='0.16.15',
+    version='0.16.16',
     description='Mundipagg API',
     long_description=long_description,
     long_description_content_type="text/markdown",
