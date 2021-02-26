9ff52460b1d99cb5dfcefe5f458fbfee293af771
Marcelo Marini <marcelomarini17@hotmail.com>
2021-02-26 16:20:08 -0300

    :art: Adds waiting_funds_amount, transferred_amount in GetBalanceResponse (#66)
    Co-authored-by: Thiago Barradas <th.barradas@gmail.com>

--- mundiapi/mundiapi_client.py
+++ mundiapi/mundiapi_client.py
@@ -8,15 +8,15 @@
 
 from mundiapi.decorators import lazy_property
 from mundiapi.configuration import Configuration
+from mundiapi.controllers.subscriptions_controller import SubscriptionsController
+from mundiapi.controllers.orders_controller import OrdersController
+from mundiapi.controllers.plans_controller import PlansController
+from mundiapi.controllers.invoices_controller import InvoicesController
 from mundiapi.controllers.customers_controller import CustomersController
 from mundiapi.controllers.charges_controller import ChargesController
 from mundiapi.controllers.recipients_controller import RecipientsController
-from mundiapi.controllers.subscriptions_controller import SubscriptionsController
-from mundiapi.controllers.invoices_controller import InvoicesController
-from mundiapi.controllers.orders_controller import OrdersController
-from mundiapi.controllers.sellers_controller import SellersController
 from mundiapi.controllers.tokens_controller import TokensController
-from mundiapi.controllers.plans_controller import PlansController
+from mundiapi.controllers.sellers_controller import SellersController
 from mundiapi.controllers.transactions_controller import TransactionsController
 from mundiapi.controllers.transfers_controller import TransfersController
 
@@ -24,6 +24,22 @@ from mundiapi.controllers.transfers_controller import TransfersController
 class MundiapiClient(object):
 
     config = Configuration
+
+    @lazy_property
+    def subscriptions(self):
+        return SubscriptionsController()
+
+    @lazy_property
+    def orders(self):
+        return OrdersController()
+
+    @lazy_property
+    def plans(self):
+        return PlansController()
+
+    @lazy_property
+    def invoices(self):
+        return InvoicesController()
 
     @lazy_property
     def customers(self):
@@ -38,28 +54,12 @@ class MundiapiClient(object):
         return RecipientsController()
 
     @lazy_property
-    def subscriptions(self):
-        return SubscriptionsController()
-
-    @lazy_property
-    def invoices(self):
-        return InvoicesController()
-
-    @lazy_property
-    def orders(self):
-        return OrdersController()
-
-    @lazy_property
-    def sellers(self):
-        return SellersController()
-
-    @lazy_property
     def tokens(self):
         return TokensController()
 
     @lazy_property
-    def plans(self):
-        return PlansController()
+    def sellers(self):
+        return SellersController()
 
     @lazy_property
     def transactions(self):
