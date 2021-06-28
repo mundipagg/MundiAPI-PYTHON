3241027692131bea28638648b26fff3943eb1e9f
Thiago Barradas <th.barradas@gmail.com>
2021-06-28 17:26:02 +0000

    added new endpoint and properties

--- mundiapi/controllers/base_controller.py
+++ mundiapi/controllers/base_controller.py
@@ -32,9 +32,7 @@ class BaseController(object):
     http_call_back = None
 
     global_headers = {
-
-        'user-agent': 'MundiSDK - Python 2.0.0'
-
+        'user-agent': 'MundiSDK - Python 2.1.0'
     }
 
     def __init__(self, client=None, call_back=None):
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='mundiapi',
-    version='2.0.0',
+    version='2.1.0',
     description='Mundipagg API',
     long_description=long_description,
     long_description_content_type="text/markdown",
